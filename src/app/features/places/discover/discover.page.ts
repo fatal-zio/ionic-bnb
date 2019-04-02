1a3e7cdd5f357849b5fcfe59244677490c3e6810
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-04-01 22:17:21 -0500

    Added dummy layout for Discovery screen.

--- src/app/features/places/discover/discover.page.ts
+++ src/app/features/places/discover/discover.page.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaceService } from '../../../core/services/place.service';
+import { Place } from '../../../shared/models/place.model';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+  styleUrls: ['./discover.page.scss']
 })
 export class DiscoverPage implements OnInit {
+  places: Place[];
 
-  constructor(private placeService: PlaceService) { }
+  constructor(private placeService: PlaceService) {}
 
   ngOnInit() {
+    this.places = this.placeService.places;
   }
-
 }
