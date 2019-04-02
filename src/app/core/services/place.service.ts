0ca1908d5b391c3d68822057f03697cb4111167d
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-04-01 20:26:50 -0500

    Added dummy data and Place model.

--- src/app/core/services/place.service.ts
+++ src/app/core/services/place.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Place } from '../../shared/models/place.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaceService {
-  private _places = [];
+  private _places: Place[] = [
+    new Place(
+      'id1',
+      'Manhattan Mansion',
+      'In the heart of New York.',
+      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
+      149.99
+    ),
+    new Place(
+      'id2',
+      'L\'Amour Toujours',
+      'Romantic in Paris',
+      'https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/03/Shangri-La-Paris-Hotel-Suite-View-2.jpg',
+      189.99
+    ),
+    new Place(
+      'id3',
+      'The Foggy Palace',
+      'Not your average city trip.',
+      'https://i1.trekearth.com/photos/138102/dsc_0681.jpg',
+      99.99
+    )
+  ];
 
   get places() {
     return [...this._places];
--- src/app/features/places/discover/discover.page.ts
+++ src/app/features/places/discover/discover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlaceService } from '../../../core/services/place.service';
 
 @Component({
   selector: 'app-discover',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
-  constructor() { }
+  constructor(private placeService: PlaceService) { }
 
   ngOnInit() {
   }
