1c81f0d18cc869daf471a47ae450a7daa5683c86
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-04-02 12:12:42 -0500

    Added navigation between Discover and Detail page.

--- src/app/features/places/discover/place-detail/place-detail.page.ts
+++ src/app/features/places/discover/place-detail/place-detail.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  styleUrls: ['./place-detail.page.scss']
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private router: Router, private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onBookPlace() {
+    this.navCtrl.navigateBack('/places/tabs/discover');
   }
-
 }
