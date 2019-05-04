dc1dfdd3caea651ab1bdd96b00eed2f8c9b3265e
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-05-04 11:54:53 -0500

    Added Offer Item component.

--- src/app/features/places/offers/offers.page.ts
+++ src/app/features/places/offers/offers.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../../../shared/models/place.model';
+import { PlaceService } from '../../../core/services/place.service';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+  styleUrls: ['./offers.page.scss']
 })
 export class OffersPage implements OnInit {
+  offers: Place[];
 
-  constructor() { }
+  constructor(private placeService: PlaceService) {}
 
   ngOnInit() {
+    this.offers = this.placeService.places;
   }
 
+  onEdit(id, slidingItem) {
+    console.log(id);
+  }
 }
