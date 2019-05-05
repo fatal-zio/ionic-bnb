9befb9a50d380d410194a8e6bdbe62f9de760e7c
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-05-04 19:04:05 -0500

    Added controls for New and Edit Offer screens.

--- src/app/features/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/features/places/offers/edit-offer/edit-offer.page.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../../../../shared/models/place.model';
+import { ActivatedRoute } from '@angular/router';
+import { PlaceService } from '../../../../core/services/place.service';
+import { NavController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  styleUrls: ['./edit-offer.page.scss']
 })
 export class EditOfferPage implements OnInit {
+  place: Place;
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private placeService: PlaceService,
+    private navController: NavController
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navController.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placeService.getPlace(paramMap.get('placeId'));
+
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
+    });
   }
 
+  onUpdateOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    console.log(this.form);
+  }
 }
