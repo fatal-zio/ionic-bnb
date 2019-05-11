39c63eaa6790ce6bf80eb25ab06285cff4b96e58
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-05-11 10:42:43 -0500

    Added some of the logic required for creating Offers.

--- src/app/core/services/place.service.ts
+++ src/app/core/services/place.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Place } from '../../shared/models/place.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +14,19 @@ export class PlaceService {
       'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
       149.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date('2019-12-31'),
+      'abc'
     ),
     new Place(
       'id2',
+      // tslint:disable-next-line: quotemark
       "L'Amour Toujours",
       'Romantic in Paris',
       'https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/03/Shangri-La-Paris-Hotel-Suite-View-2.jpg',
       189.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date('2019-12-31'),
+      'fdr'
     ),
     new Place(
       'id3',
@@ -31,17 +35,39 @@ export class PlaceService {
       'https://i1.trekearth.com/photos/138102/dsc_0681.jpg',
       99.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date('2019-12-31'),
+      'jdp'
     )
   ];
+
+  constructor(private authService: AuthService) {}
+
+  getPlace(id: string): Place {
+    return { ...this._places.find(p => p.id === id) };
+  }
 
   get places() {
     return [...this._places];
   }
 
-  constructor() {}
+  addPlace(
+    title: string,
+    description: string,
+    price: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    const newPlace = new Place(
+      Math.random().toString(),
+      title,
+      description,
+      'https://i1.trekearth.com/photos/138102/dsc_0681.jpg',
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId
+    );
 
-  getPlace(id: string): Place {
-    return { ...this._places.find(p => p.id === id) };
+    this._places.push(newPlace);
   }
 }
--- src/app/features/places/offers/new-offer/new-offer.page.ts
+++ src/app/features/places/offers/new-offer/new-offer.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PlaceService } from '../../../../core/services/place.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-offer',
@@ -8,7 +10,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class NewOfferPage implements OnInit {
   form: FormGroup;
-  constructor() {}
+  constructor(private placeService: PlaceService, private router: Router) {}
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -39,6 +41,16 @@ export class NewOfferPage implements OnInit {
     if (!this.form.valid) {
       return;
     }
-    console.log(this.form);
+
+    this.placeService.addPlace(
+      this.form.value.title,
+      this.form.value.description,
+      +this.form.value.price,
+      new Date(this.form.value.dateFrom),
+      new Date(this.form.value.dateTo)
+    );
+
+    this.form.reset();
+    this.router.navigate(['/places/tabs/offers']);
   }
 }
